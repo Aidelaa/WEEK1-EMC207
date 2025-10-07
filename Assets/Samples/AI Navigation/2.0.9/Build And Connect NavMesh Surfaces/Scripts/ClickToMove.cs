e85b58be47271de638327ebca578ad0afefe35f3
Allysa <alilyfad26@gmail.com>
2025-10-07 10:40:54 +0800

    week5

--- Assets/Samples/AI Navigation/2.0.9/Build And Connect NavMesh Surfaces/Scripts/ClickToMove.cs
+++ Assets/Samples/AI Navigation/2.0.9/Build And Connect NavMesh Surfaces/Scripts/ClickToMove.cs
@@ -11,10 +11,14 @@ namespace Unity.AI.Navigation.Samples
     {
         NavMeshAgent m_Agent;
         RaycastHit m_HitInfo = new RaycastHit();
+        private Animator animation;
+        public NavMeshAgent agent;
+
 
         void Start()
         {
             m_Agent = GetComponent<NavMeshAgent>();
+            animation = GetComponent<Animator>();
         }
 
         void Update()
@@ -24,6 +28,23 @@ namespace Unity.AI.Navigation.Samples
                 var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 if (Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
                     m_Agent.destination = m_HitInfo.point;
+            }
+
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                animation.SetBool("Running",false);
+            }
+            else
+            {
+                animation.SetBool("Running",true);
+            }
+        }
+
+        void OnAnimatorMove()
+        {
+            if(animation.GetBool("Running"))
+            {
+                m_Agent.speed = (animation.deltaPosition/Time.deltaTime).magnitude;
             }
         }
     }
